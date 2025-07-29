027babc620f879c9c2772f0f6dcfe35464228bda
LittleHaku <ivandelhorno@gmail.com>
2025-07-29 12:49:43 +0200

    quitado millionbot

--- tracer/connectors/chatbot_connectors/__init__.py
+++ tracer/connectors/chatbot_connectors/__init__.py
@@ -12,28 +12,21 @@ from .core import (
     SimpleTextProcessor,
 )
 from .factory import ChatbotFactory
-from .implementations.millionbot import ChatbotAdaUam, MillionBot, MillionBotConfig
 from .implementations.rasa import RasaChatbot, RasaConfig
 from .implementations.taskyto import ChatbotTaskyto, TaskytoConfig
 
 # Register all chatbot implementations with the factory
-ChatbotFactory.register_chatbot("taskyto", ChatbotTaskyto, requires_url=True, description="Taskyto chatbot connector")
-ChatbotFactory.register_chatbot(
-    "ada-uam", ChatbotAdaUam, requires_url=False, description="Pre-configured ADA UAM chatbot"
-)
-ChatbotFactory.register_chatbot("rasa", RasaChatbot, requires_url=True, description="RASA chatbot connector")
+ChatbotFactory.register_chatbot("taskyto", ChatbotTaskyto, description="Taskyto chatbot connector")
+ChatbotFactory.register_chatbot("rasa", RasaChatbot, description="RASA chatbot connector")
 
 __all__ = [
     "Chatbot",
-    "ChatbotAdaUam",
     "ChatbotConfig",
     "ChatbotFactory",
     "ChatbotResponse",
     "ChatbotTaskyto",
     "EndpointConfig",
     "Headers",
-    "MillionBot",
-    "MillionBotConfig",
     "Payload",
     "RasaChatbot",
     "RasaConfig",
--- tracer/connectors/chatbot_connectors/implementations/__init__.py
+++ tracer/connectors/chatbot_connectors/implementations/__init__.py
@@ -1,14 +1,10 @@
 """Chatbot implementation modules."""
 
-from .millionbot import ChatbotAdaUam, MillionBot, MillionBotConfig
 from .rasa import RasaChatbot, RasaConfig
 from .taskyto import ChatbotTaskyto, TaskytoConfig
 
 __all__ = [
-    "ChatbotAdaUam",
     "ChatbotTaskyto",
-    "MillionBot",
-    "MillionBotConfig",
     "RasaChatbot",
     "RasaConfig",
     "TaskytoConfig",
